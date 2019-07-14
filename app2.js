500f7dd4dacd3af30ed9307bb023c0f426bde66c
Jonathan Jacobsen <jonathan.jacobsen@gmail.com>
2019-07-14 17:42:26 -0400

    Continued 07-14-2019

--- app2.js
+++ app2.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var app = express();
 
-'''Need to commit
+//Need to commit
 app.get('/', function(req, res) {
     res.send('Hello World!');
 });
 
 app.listen(3000, function() {
     console.log('Example app listening on port 3000!');
+});
+
+app.all('/secret', function(req, res, next) {
+    console.log('Accessing the secret section ...');
+    next(); // pass control to the next handler
 });
